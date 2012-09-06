ea59b99050ad9baef7b64770754787af42602a72
nokuno <nokuno@nokuno.jp>
2012-09-06 07:44:48 +0100

    refactoring

--- src/table.h
+++ src/table.h
@@ -1,7 +1,10 @@
-#ifndef TAbLE_H
-#define TAbLE_H
+#ifndef TABLE_H
+#define TABLE_H
 
 namespace jsc {
+
+// Table implements rule-based conversion such as
+// romaji-to-kana conversion etc.
 
 class Table : private map<string, string> {
 public:
